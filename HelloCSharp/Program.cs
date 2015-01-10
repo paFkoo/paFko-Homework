e1917a62b49fc573f4834387f9e60373f1aa4645
Pavel <MakeMyDaybg@gmail.com>
2015-01-10 04:55:55 +0200

    add

---
+++ HelloCSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloCSharp
+//Create, compile and run a “Hello C#” console application.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloC#");
+        }
+    }
+}
